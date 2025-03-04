657e28e0ccde9b5fd80fbb88245ad371eddd860b
bitshiftmod <bitshiftmod@gmail.com>
2025-03-03 23:38:52 -0500

    chore: update base

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -9,6 +9,7 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), nodePolyfills({ include: [] }), svgr()],
+  base: ".",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
